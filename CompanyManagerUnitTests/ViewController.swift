3f4a6c82fba5ee9400bbf9d24b90993847cfde1c
Kamil <kamil@macbook-pro-kamil.local>
2019-03-01 16:54:51 +0100

    Initial Commit

---
+++ CompanyManagerUnitTests/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CompanyManagerUnitTests
+//
+//  Created by Kamil on 01/03/2019.
+//  Copyright © 2019 Kamil Zając. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
